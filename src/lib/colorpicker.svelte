6b533c72791ba758dcdda711ad76cd1778444661
headmad <kov.ps@ya.ru>
2024-12-21 13:55:55 +0800

    add alpha chenel

--- src/lib/colorpicker.svelte
+++ src/lib/colorpicker.svelte
@@ -1,5 +1,7 @@
 <script>
 
+	let {onchangecolor, startcolor} = $props();
+
 	let hR = $state(255);
 	let hG = $state(0);
 	let hB = $state(0);
@@ -7,18 +9,22 @@
 	let cx = $state(0);
 	let cy = $state(0);
 	let hy = $state(0);
+	let ay = $state(0);
 
 	let color = $state();
 	let hue = $state();
+	let alpha = $state();
 	
 	let R = $state(0);
 	let G = $state(0);
 	let B = $state(0);
+	let A = $state(1);
 
-	let tmp = $state(0);
+
 
 	let isColorMouseDown = $state(false);
 	let isHueMouseDown = $state(false);
+	let isMouseDown = $state(false);
 
 
 	const hueSections = [
@@ -37,44 +43,42 @@
 		setRGB();
 	}
 
+	
+	
+	let hueSectionHeight = 0;
+	let mouseDownHandlers = new Map();
+	
+	$effect(() => {
+		hueSectionHeight = hue?.offsetHeight / 6;
+
+		mouseDownHandlers.set(color, colorMouseMove);
+		mouseDownHandlers.set(hue, hueMouseMove);
+		mouseDownHandlers.set(alpha, alphaMouseMove);
+
+		
+		document.addEventListener('mousedown', (e) => {
+			isMouseDown = e.target;
+			
+			if (mouseDownHandlers.has(isMouseDown)) 
+				mouseDownHandlers.get(isMouseDown)(e);
+			
+		});
+		
+		document.addEventListener('mouseup', (e) => {
+			isMouseDown = false;
+		});
+
+		document.addEventListener('mousemove', (e) => {
+			if (mouseDownHandlers.has(isMouseDown)) 
+				mouseDownHandlers.get(isMouseDown)(e);
+		});
+	});
+
 	$effect(() => {
 		if (isColorMouseDown || isHueMouseDown)
 			document.body.classList.add('unselectable');
 		else
 			document.body.classList.remove('unselectable');
-	});
-	
-	let hueSectionHeight = 0;
-	
-	$effect(() => {
-		hueSectionHeight = hue?.offsetHeight / 6;
-		
-		document.addEventListener('mousedown', (e) => {
-			if (e.target == color) {
-				isColorMouseDown = true;
-				colorMouseMove(e);
-			}
-
-			if (e.target == hue) {
-				isHueMouseDown = true;
-				hueMouseMove(e);
-			}
-			
-		});
-
-		document.addEventListener('mouseup', (e) => {
-			isColorMouseDown = false;
-			isHueMouseDown = false;
-		});
-
-		document.addEventListener('mousemove', (e) => {
-			if (isColorMouseDown)
-				colorMouseMove(e);
-
-			
-			if (isHueMouseDown)
-				hueMouseMove(e);
-		});
 	});
 
 	function colorMouseMove(e) {
@@ -89,7 +93,6 @@
 		const h = color.offsetHeight;
 		const w = color.offsetWidth;
 		const setColor = (hRGB) => {
-			// return hRGB + valueInRange(255 - hRGB, 255, 255 - cx);
 			const xRGB = hRGB + valueInRange(255 - hRGB, w, w - cx);
 			return  Math.round(valueInRange(xRGB * 255, h, h - cy)/255);
 		};
@@ -108,16 +111,23 @@
 		hueSections[s](val);
 	}
 
+	function alphaMouseMove(e) {
+		const coord = getCoordinates(e, alpha);
+		ay = coord.y;
+
+		A = Number(1 - ay/alpha.offsetHeight).toFixed(2);
+	}
+
 	function getCoordinates(e, target) {
 		const box = target.getBoundingClientRect();
 		let x, y;
 		return {
 		 get y() {
-		   y = Math.ceil(e.pageY - box.top + window.pageYOffset);
+		   y = Math.ceil(e.pageY - box.top - window.pageYOffset);
 			 return clamp(0, target.offsetHeight, y);
 		 },
 		 get x() {
-		   x = Math.ceil(e.pageX - box.left + window.pageXOffset);
+		   x = Math.ceil(e.pageX - box.left - window.pageXOffset);
 			 return clamp(0, target.offsetWidth, x);
 		 }
 		};
@@ -135,16 +145,16 @@ function valueInRange(newMax, curMax, curValue) {
 
 </script>
 <div class="rgb">
-<span class="square" style="background: rgb({R}, {G}, {B})"></span>
+<span class="square" style="background: rgba({R}, {G}, {B}, {A})"></span>
 <span></span>
 </div>
-R: [{R}] G: [{G}] B: [{B}]
+R: [{R}] G: [{G}] B: [{B}] A: [{A}]
 <br>
 hR: [{hR}] hG: [{hG}] hB: [{hB}]
 <br>
 cy: {cy} cx: {cx}
 
-<div class="body">
+<div class="body" style="--color: rgb({R}, {G}, {B})">
 	<div
 		class="color"
 		style="background-color: rgb({hR}, {hG}, {hB})"
@@ -157,12 +167,21 @@ cy: {cy} cx: {cx}
 			top:{cy}px;
 			"></div>
 		</div>
+
 	<div
 		class="hue"
 		class:nocursor={isHueMouseDown}
 		bind:this={hue}
 		>
-		<div class="hue-picker" style="top:{hy}px"></div>
+		<div class="hue-picker" style="top:{hy}px; left: 50%;"></div>
+		</div>
+
+		<div
+		class="alpha"
+		class:nocursor={isHueMouseDown}
+		bind:this={alpha}
+		>
+		<div class="alpha-picker" style="top:{ay}px; left: 50%;"></div>
 		</div>
 </div>
 
@@ -193,10 +212,9 @@ cy: {cy} cx: {cx}
 	}
 	.hue {
 		position: relative;
-		width: 40px;
+		width: 20px;
 		height: 240px;
 		background-image: linear-gradient(0deg, red 0, #f0f 17%, #00f 33%, #0ff 50%, #0f0 67%, #ff0 83%, red);
-		/* background-image: linear-gradient(0deg, red 0, #ff0 17%, #0f0 33%, #0ff 50%, #00f 67%, #f0f 83%, red); */
 	}
 	.color {
 		width: 240px;
@@ -204,7 +222,16 @@ cy: {cy} cx: {cx}
 		position: relative;
 		background-color: #ff0000;
 		background-image: linear-gradient(0deg, #000, rgba(204, 154, 129, 0)),
-			linear-gradient(90deg, #fff, rgba(204, 154, 129, 0));
+			linear-gradient(90deg, #fff, rgba(255, 255, 255, 0));
+	}
+
+	.alpha {
+		position: relative;
+		width: 20px;
+		height: 240px;
+		background-image: linear-gradient(0deg, #ffffff00, var(--color)),
+		url("data:image/svg+xml,%3Csvg viewBox='0 0 2 2' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='1' height='1' fill='%230000001a'/%3E%3Crect x='1' y='1' width='1' height='1' fill='%230000001a'/%3E%3C/svg%3E");
+		background-size: 10px;
 	}
 
 	.nocursor {
@@ -212,18 +239,18 @@ cy: {cy} cx: {cx}
 	}
 
 
-	.color-picker {
+	.color-picker, .hue-picker, .alpha-picker {
 		position: absolute;
 		width: 8px;
 		height: 8px;
 		border: 2px solid #fff;
-		box-shadow: 0 0 3px rgba(0, 0, 0, .3);
+		box-shadow: 0 0 3px  rgba(0, 0, 0, .6);
 		border-radius: 50%;
 		transform: translate(-50%, -50%);
 	}
 
 
-	.hue-picker {
+	/* .hue-picker {
 		position: absolute;
 		width: 100%;
 		height: 1px;
@@ -233,7 +260,7 @@ cy: {cy} cx: {cx}
 		border: 1px solid #fff;
 		transform: translateY(-50%);
 		opacity: 1;
-	}
+	} */
 
 	
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <script>
    import Colorpicker from '$lib/colorpicker.svelte';
+
+   let R = $state(0);
+	let G = $state(0);
+	let B = $state(0);
+
+  function onchangecolor ({RGBA, HEX, HSLA}) {
+    [R, G, B] = RGB;
+  }
 </script>
 
 <main>
-  <Colorpicker/>
+  R: [{R}] G: [{G}] B: [{B}]
+  <Colorpicker />
 </main>
 
 <style>
